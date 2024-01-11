2f9ee12cf036d0cb4e6d67a1317a54747d2d68a5
Jack Gardner <jack_gardner123@hotmail.com>
2024-01-11 22:16:15 +0000

    Fix bug with looping through episodes wrongly

--- script.py
+++ script.py
@@ -13,26 +13,30 @@ def main():
     load_dotenv()
     SPOTIFY_PLAYLIST_ID = os.getenv('SPOTIFY_PLAYLIST_ID')
     episodes: List[EpisodeInfo] = ListEpisodes()
-    for episode in episodes:
-        # take only first two 
-        episodes = episodes[:2]
 
-        for episode in episodes:
-            print(f"Episode: {episode['episode']}")
-            print(f"Link: {episode['episode_link']}")
-            music_tracks = FindMusicPlayed(episode['episode_link'])
-            for track in music_tracks:
-                print(f" - {track.get('artist')} - {track['song']}")
-                spotify_tracks = search_song(track['song'], track.get('artist', ''))
-                if spotify_tracks:
-                    spotify_id = spotify_tracks[0]['external_urls']['spotify'].split('/')[-1]
-                    print(f"Spotify ID: {spotify_id}")
-                    # Check if the track is already in the playlist
-                    if not is_track_in_playlist(spotify_id, SPOTIFY_PLAYLIST_ID):
-                        # Add the track to the playlist if it's not there
-                        add_song_to_playlist(spotify_id, SPOTIFY_PLAYLIST_ID)
-                        print(f"Added to playlist: {SPOTIFY_PLAYLIST_ID}")
-            print("\n")
+    # Filter the episodes to make sure they have unique links
+    filtered_episodes: List[EpisodeInfo] = []
+
+    for episode in episodes:
+        if episode['episode_link'] not in [item['episode_link'] for item in filtered_episodes]:
+            filtered_episodes.append(episode)
+
+    for episode in filtered_episodes:
+        print(f"Episode: {episode['episode']}")
+        print(f"Link: {episode['episode_link']}")
+        music_tracks = FindMusicPlayed(episode['episode_link'])
+        for track in music_tracks:
+            print(f" - {track.get('artist')} - {track['song']}")
+            spotify_tracks = search_song(track['song'], track.get('artist', ''))
+            if spotify_tracks:
+                spotify_id = spotify_tracks[0]['external_urls']['spotify'].split('/')[-1]
+                print(f"Spotify ID: {spotify_id}")
+                # Check if the track is already in the playlist
+                if not is_track_in_playlist(spotify_id, SPOTIFY_PLAYLIST_ID):
+                    # Add the track to the playlist if it's not there
+                    add_song_to_playlist(spotify_id, SPOTIFY_PLAYLIST_ID)
+                    print(f"Added to playlist: {SPOTIFY_PLAYLIST_ID}")
+        print("\n")
 
 # Execute the script
 if __name__ == "__main__":
